064d2904a3735bd1012251da6aba182c48ce7c9e
users1234321 <1400280785@qq.com>
2026-02-22 17:31:21 +0800

    feat: design main application shell and navigation logic

---
+++ include/Hosp_Reg_UI.h
@@ -0,0 +1,32 @@
+﻿
+// Hosp_Reg_UI.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHospRegUIApp:
+// 有关此类的实现，请参阅 Hosp_Reg_UI.cpp
+//
+
+class CHospRegUIApp : public CWinApp
+{
+public:
+	CHospRegUIApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHospRegUIApp theApp;
---
+++ include/Hosp_Reg_UIDlg.h
@@ -0,0 +1,40 @@
+﻿
+// Hosp_Reg_UIDlg.h: 头文件
+//
+#include "SQLInterface.h"
+
+#pragma once
+
+
+// CHospRegUIDlg 对话框
+class CHospRegUIDlg : public CDialogEx
+{
+// 构造
+public:
+	CHospRegUIDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HOSP_REG_UI_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	SQLInterface SQL;
+
+	afx_msg void OnBnClickedButtonUser();
+	afx_msg void OnBnClickedButtonAdmin();
+};
