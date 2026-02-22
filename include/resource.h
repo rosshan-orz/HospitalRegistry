8c5100ce07d7a0768ee6670cfb438f688e465729
rosshan-orz <b23020028@njupt.edu.cn>
2026-02-22 17:43:29 +0800

    feat: design main application shell and navigation logic

---
+++ include/resource.h
@@ -0,0 +1,93 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 HospRegUI.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_HOSP_REG_UI_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_UserLogin                   130
+#define IDD_AdminLogin                  132
+#define IDD_Order                       133
+#define IDD_DocArrange                  134
+#define IDD_UserReg                     138
+#define IDD_AdminReg                    139
+#define IDD_DocInfo                     146
+#define IDD_DocWorkday                  148
+#define IDD_DocOrderState               150
+#define IDD_UserInfo                    151
+#define IDD_MyOrder                     154
+#define IDD_History                     156
+#define IDC_BUTTON_User                 1001
+#define IDC_BUTTON_Admin                1002
+#define IDC_BUTTON_UserExit             1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT_DocID                  1004
+#define IDC_EDIT2                       1005
+#define IDC_EDIT_DocName                1005
+#define IDC_EDIT_phone_num              1005
+#define IDC_BUTTON_UserReg              1006
+#define IDC_EDIT4                       1006
+#define IDC_EDIT_UserID                 1006
+#define IDC_BUTTON_UserLogin            1007
+#define IDC_EDIT5                       1007
+#define IDC_EDIT_Name                   1007
+#define IDC_BUTTON_AdminLogin           1011
+#define IDC_BUTTON_AdminReg             1012
+#define IDC_BUTTON_AdminExit            1013
+#define IDC_BUTTON_UserInfo             1015
+#define IDC_BUTTON_Sum                  1016
+#define IDC_BUTTON_DocInfo              1017
+#define IDC_BUTTON_DocArrange           1020
+#define IDC_BUTTON_DocOrder             1021
+#define IDC_EDIT_Token                  1022
+#define IDC_BUTTON_AdminRegCon          1023
+#define IDC_BUTTON_AdminRegExit         1024
+#define IDC_EDIT_AdminAccount           1027
+#define IDC_EDIT_AdminPwd               1028
+#define IDC_LIST1                       1029
+#define IDC_BUTTON_CheckDoc             1030
+#define IDC_BUTTON_MyOrder              1031
+#define IDC_BUTTON_History              1032
+#define IDC_EDIT_UserAccount            1033
+#define IDC_EDIT_UserPwd                1034
+#define IDC_COMBO_DocDepart             1036
+#define IDC_COMBO_DocPos                1037
+#define IDC_BUTTON_DocInfoOK            1038
+#define IDC_BUTTON_Change               1038
+#define IDC_BUTTON_Exit                 1039
+#define IDC_CHECK_Mon                   1041
+#define IDC_CHECK_Tue                   1042
+#define IDC_CHECK_Wed                   1043
+#define IDC_CHECK_Thur                  1044
+#define IDC_CHECK_Fri                   1045
+#define IDC_LIST_DocWorkday             1046
+#define IDC_BUTTON_DocWorkdayOK         1047
+#define IDC_COMBO1                      1051
+#define IDC_COMBO_Day                   1051
+#define IDC_COMBO_Time                  1052
+#define IDC_BUTTON_Order                1053
+#define IDC_BUTTON_Add                  1053
+#define IDC_BUTTON2_Exit                1055
+#define IDC_BUTTON_CancelOrder          1056
+#define IDC_BUTTON_Del                  1057
+#define IDC_LIST_History                1059
+#define IDC_LIST_DocInfo                1061
+#define IDC_BUTTON_Load                 1062
+#define IDC_LIST_UserInfo               1063
+#define IDC_BUTTON1                     1067
+#define IDC_LIST_MyOrder                1068
+#define IDC_EDIT_OrderID                1069
+#define IDC_DATETIMEPICKER1             1070
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        160
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1071
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
